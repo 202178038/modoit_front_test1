a5837f5f922746597e0c72129310de1df543d74c
202178038 <202178038@yiu.ac.kr>
2025-07-16 22:22:42 +0900

    feat: 유진이 코드 받고 서버 배포를 위해 수정

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'yiu-moduit-app',
+    name: 'yiu-modoit-app',
     slug: 'yiu-moduit-app',
     version: '1.0.0',
     orientation: 'portrait',
@@ -18,6 +18,7 @@ export default {
       supportsTablet: true,
     },
     android: {
+      package: "com.bswon33.yiumoduitapp", // ✅ 여기에 추가!
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
@@ -27,9 +28,11 @@ export default {
     web: {
       favicon: './assets/favicon.png',
     },
-
     extra: {
-      BASE_URL: process.env.BASE_URL,
+      BASE_URL: process.env.EXPO_PUBLIC_BASE_URL, // 배포를 위해 변경 !
+      eas: {
+        projectId: '0f94382b-79ae-424d-80c7-4cd07bb41ad2', // 새로 추가 !
+      },
     },
   },
 };
