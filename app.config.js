7100d3ef5f319a93526eacdd3977e81a443cfd26
202178038 <202178038@yiu.ac.kr>
2025-08-02 23:55:16 +0900

    fix: 파일 2개 수정

--- app.config.js
+++ app.config.js
@@ -18,7 +18,8 @@ export default {
       supportsTablet: true,
     },
     android: {
-      package: "com.bswon33.yiumoduitapp", // ✅ 여기에 추가!
+      package: "com.AISL.Modoit", // ✅ 여기에 추가!
+      versionCode: 1, // ← 이거 명시하는 걸 권장!
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
